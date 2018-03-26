3617daaeb7171d16aa38853e89a3e2265c383e23
Willie Smalls <williesmalls@Willies-MacBook-Air.local>
2018-03-25 23:45:56 -0400

    introduced reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,7 @@ function genRandomNumInterval(min,max)
     return Math.floor(Math.random()*(max-min+1)+min);
 }
 
+//target number
 var targetNumber = 0;
 
 //load document and begin by displaying the random number to match
@@ -18,25 +19,59 @@ var randomNumber2 = genRandomNumInterval(1,12);
 var randomNumber3 = genRandomNumInterval(1,12);
 var randomNumber4 = genRandomNumInterval(1,12);
 
-var totalScore = 0; 
+//initial variables
+var totalScore = 0;
+var win = 0 
+var loss = 0  
+
+//Reset Game variables
+function resetGame(){
+    //reset target number
+    targetNumber = genRandomNumInterval(19,127);
+    //update ui with new target number
+    $('h2#randomNumber').html(function(){ return '<h2>Random Number: '+ targetNumber +'</h2>'});
+
+    //update all random variables for pictures
+    randomNumber1 = genRandomNumInterval(1,12);
+    randomNumber2 = genRandomNumInterval(1,12);
+    randomNumber3 = genRandomNumInterval(1,12);
+    randomNumber4 = genRandomNumInterval(1,12);
+}
 
 //function to determin win or loss
 function winLossFncn(totalScore, targetNumber){
     if(targetNumber === totalScore){
+        //Display the Win Message if the player successfully reaches the target number
         $('h3#roundMsg').html(function(){ return '<h3>You Win!</h3>'});
+        //Counter for wins
+        win++;
+        //Display the number of wins
+        $('h3#numWins').html(function(){ return '<h3>Wins: '+win+'</h3>'});
+        //Reset the game
     }
+
     if (totalScore > targetNumber){ 
+        //Display the Loss Message if the player successfully reaches the target number
         $('h3#roundMsg').html(function(){ return '<h3>You Lose!</h3>'}); 
+        //Counter for losses
+        loss++;
+        //Display the number of losses
+        $('h3#numLosses').html(function(){ return '<h3>Losses: '+loss+'</h3>'});
+        //Reset the game
     }
 }
 
+//Function for updating the total score
+function updateTotalScore(totalScore){
+    $('h2#totalScore').html(function(){ return '<h2>Total Score: '+ totalScore +'</h2>'});
+}
+
+//Image One Function
 $('img#random_one').on('click', function(){
     //tally score
     totalScore = totalScore + randomNumber1;
-    
     //update the UI
-    $('h2#totalScore').html(function(){ return '<h2>Total Score: '+ totalScore +'</h2>'});
-
+    updateTotalScore(totalScore);
     //run function for win or loss
     winLossFncn(totalScore, targetNumber);
 });
